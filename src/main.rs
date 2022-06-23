40fbcf4931086cbb96a9f544f510ab37da5b75ad
huy-spm <nguyenhuy2712000@gmail.com>
2022-06-23 23:27:40 +0700

    Xong

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,56 @@
 
 
 
-// fn main(){
-//     println!("Hello, world!");
-//     fibonacci_numbers();
-//     for number in fibonacci_numbers() {
-//         println!("{}", number);
-// }
+fn main(){
+    println!("Hello, world!");
+    for number in fibonacci_numbers() {
+        println!("{}", number);
+}
+}
+#[derive(Debug)]
+struct Fibonacci {
+    a: u32,
+    b: u32,
+}
 
-// #[derive(Debug)]
-// struct Fibonacci {
-//     a: u32,
-//     b: u32,
-// }
 
-// impl Iterator for Fibonacci {
-//     type Item = u32;
+impl Iterator for Fibonacci {
+    type Item = u32;
 
-//     fn next(&mut self) -> Option<u32> {
-//         Some(self.a+self.b)
-//     }
-// }
+    fn next(&mut self) -> Option<u32> {
+        let new_a = self.a + self.b;
 
-// fn fibonacci_numbers() -> Fibonacci {
-//     Fibonacci { a: 1, b: 0 }
-// }
+		self.b= self.a;
+		self.a = new_a;
+
+		Some(self.a)
+    }
+}
+
+fn fibonacci_numbers() -> Fibonacci {
+    Fibonacci { a: 1, b: 0 }
+}
 
 
 // Bài 2: Lifetime
 // Yêu cầu: Sửa lỗi Lifetime 
 
-use std::fmt;
+// use std::fmt;
 
-#[derive(Debug)]
-struct  StrDisplayable (Vec<&'static str>);
+// #[derive(Debug)]
+// struct  StrDisplayable (Vec<&'static str>);
 
-impl fmt::Display for StrDisplayable{
-    fn fmt<'a>(&'a self, f: &'a mut fmt::Formatter) ->fmt::Result {
-        for v in &self.0 {
-            write!(f, "\n{}",v)?;
-        }
-        Ok(())
-    }
-}
+// impl fmt::Display for StrDisplayable{
+//     fn fmt<'a>(&'a self, f: &'a mut fmt::Formatter) ->fmt::Result {
+//         for v in &self.0 {
+//             write!(f, "\n{}",v)?;
+//         }
+//         Ok(())
+//     }
+// }
 
-fn main() {
-        let vec: Vec<&str> = vec!["a","bc","def"];
-        let vec_Foo = StrDisplayable(vec);
-        println!("{}",vec_Foo);
-}
+// fn main() {
+//         let vec: Vec<&str> = vec!["a","bc","def"];
+//         let vec_Foo = StrDisplayable(vec);
+//         println!("{}",vec_Foo);
+// }
